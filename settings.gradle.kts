f17c2a89b0a2729ce72d979b79a57d734bedd975
linfuhui272722 <161229783+linfuhui272722@users.noreply.github.com>
2026-07-26 19:35:53 +0800

    Configure fabric-loom plugin resolution and Java version
    Add resolution strategy for fabric-loom plugin and set Java version.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,24 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "fabric-loom") {
+                useModule("net.fabricmc:fabric-loom:${requested.version}")
+            }
+        }
+    }
 }
-
+gradle.beforeProject {
+    buildscript {
+        configurations.all {
+            attributes {
+                attribute(
+                    org.gradle.api.attributes.java.TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE,
+                    21 // 假装我们需要 Java 21，这样就能匹配到 fabric-loom 的变体
+                )
+            }
+        }
+    }
+}
 rootProject.name = "MetallumShaders"
