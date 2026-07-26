2241f19849587e70714abc3117161b01ec3c9aa4
linfuhui272722 <161229783+linfuhui272722@users.noreply.github.com>
2026-07-26 19:48:39 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,10 +10,4 @@ pluginManagement {
 // settings.gradle.kts
 
 
-// 声明构建所需的 Java 工具链（给构建脚本和插件用）
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
-    }
-}
 rootProject.name = "MetallumShaders"
